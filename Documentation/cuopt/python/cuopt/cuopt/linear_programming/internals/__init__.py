3b3b9d5e974a5ab786e0040ab3347ba08829465c
techavenger123 <mihirmithani123@gmail.com>
2026-05-05 12:52:18 +0530

    Docker Config

--- Documentation/cuopt/python/cuopt/cuopt/linear_programming/internals/__init__.py
+++
@@ -1,7 +0,0 @@
-# SPDX-FileCopyrightText: Copyright (c) 2020-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
-
-from cuopt.linear_programming.internals.internals import (
-    GetSolutionCallback,
-    SetSolutionCallback,
-)
--- Documentation/cuopt/python/libcuopt/libcuopt/__init__.py
+++
@@ -1,7 +0,0 @@
-# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
-
-from libcuopt._version import __git_commit__, __version__
-from libcuopt.load import load_library
-
-__all__ = ["__git_commit__", "__version__", "load_library"]
